9ee649fde3752b28d06bebfd197c8d1e527b9a0d
Emmanuel Alozie <emmanuelalozie@Emmanuels-MacBook-Pro.local>
2024-06-12 15:46:14 +0100

    style: fix horizontal scrolling on home page
    The horizontal scrolling on the home page has been fixed by adding overflow-x-hidden to the main container.

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,7 +5,7 @@ import Story from "@/components/container/landing-page/story";
 
 export default function HomePage() {
   return (
-    <main className="min-h-[80vh] py-10 md:pt-0 md:pb-10">
+    <main className="min-h-[80vh] py-10 md:pt-0 md:pb-10 overflow-x-hidden">
       <Hero />
       <div className="px-5 md:px-10 lg:px-14 xl:px-16 2xl:px-20 pb-20 pt-32 md:pt-0 space-y-40">
         <ShowCase />
